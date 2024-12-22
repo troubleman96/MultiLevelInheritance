836174e717829d24b32c585abf169bfdea7fb340
troubleman96 <itslugenge96@gmail.com>
2024-12-22 16:25:42 +0300

    first commit

---
+++ Multilevel_Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating");
+    }
+}
+
+public class Mammal : Animal
+{
+    public void Walk()
+    {
+        Console.WriteLine("Mammal is walking...");
+    }
+}
+
+public class Dog : Mammal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog is barking!");
+    }
+}
+
+class MultilevelInheritance
+{
+    static void Main(string[] args)
+    {
+        Dog myDog = new Dog();
+        myDog.Walk();
+        myDog.Eat();
+        myDog.Bark();
+    }
+}
+
